1b11e092908b092c8f23f736d8cf4c29eda6d310
Louwind <46799905+Louwind@users.noreply.github.com>
2020-09-12 14:43:39 -0700

    Add: Cycawler Beauty reduces lightning bolt damage

--- src/main/java/github/KingVampyre/DeepTrenches/core/mixin/LivingEntityMixin.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/mixin/LivingEntityMixin.java
@@ -10,38 +10,53 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static github.KingVampyre.DeepTrenches.core.init.StatusEffects.CYCAWLER_BEAUTY;
 import static github.KingVampyre.DeepTrenches.core.init.StatusEffects.SOFTBONES;
 import static net.minecraft.entity.damage.DamageSource.*;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
 
-    @Shadow protected abstract void applyDamage(DamageSource source, float amount);
+    @Shadow
+    protected abstract void applyDamage(DamageSource source, float amount);
 
-    @Shadow public abstract boolean damage(DamageSource source, float amount);
+    @Shadow
+    public abstract boolean damage(DamageSource source, float amount);
 
     @Inject(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;applyDamage(Lnet/minecraft/entity/damage/DamageSource;F)V"), cancellable = true)
     private void applyDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         LivingEntity living = (LivingEntity) (Object) this;
 
-        if(living.hasStatusEffect(SOFTBONES)) {
+        if (living.hasStatusEffect(SOFTBONES)) {
             StatusEffectInstance effectInstance = living.getActiveStatusEffects().get(SOFTBONES);
             int amplifier = effectInstance.getAmplifier();
 
-            if (source == ANVIL)
-                this.applyDamage(source, MathHelper.ceil(amplifier > 0 ? amount * 1.5F : amount * 1.35F));
+            if (!living.isInvulnerableTo(source)) {
+                if (source == ANVIL)
+                    this.applyDamage(source, MathHelper.ceil(amplifier > 0 ? amount * 1.5F : amount * 1.35F));
 
-            if (source == CRAMMING || source == IN_WALL)
-                this.applyDamage(source, MathHelper.ceil(amplifier > 0 ? amount * 1.75F : amount * 1.35F));
+                if (source == CRAMMING || source == IN_WALL)
+                    this.applyDamage(source, MathHelper.ceil(amplifier > 0 ? amount * 1.75F : amount * 1.35F));
 
-            if (source == FALL)
-                this.applyDamage(source, MathHelper.ceil(amplifier > 0 ? amount * 1.65F : amount * 1.4F));
+                if (source == FALL)
+                    this.applyDamage(source, MathHelper.ceil(amplifier > 0 ? amount * 1.65F : amount * 1.4F));
 
-            if (source == FLY_INTO_WALL)
-                this.applyDamage(source, amount = MathHelper.ceil(amplifier > 0 ? amount * 1.45F : amount * 1.35F));
+                if (source == FLY_INTO_WALL)
+                    this.applyDamage(source, amount = MathHelper.ceil(amplifier > 0 ? amount * 1.45F : amount * 1.35F));
 
-            if(source == ANVIL || source == CRAMMING || source == IN_WALL || source == FALL || source == FLY_INTO_WALL)
+                if (source == ANVIL || source == CRAMMING || source == IN_WALL || source == FALL || source == FLY_INTO_WALL)
+                    cir.cancel();
+            }
+        }
+
+        if(living.hasStatusEffect(CYCAWLER_BEAUTY)) {
+
+            if (!living.isInvulnerableTo(source) && source == LIGHTNING_BOLT) {
+                this.applyDamage(source, MathHelper.ceil(amount * 0.1F));
+
                 cir.cancel();
+            }
+
         }
 
     }
