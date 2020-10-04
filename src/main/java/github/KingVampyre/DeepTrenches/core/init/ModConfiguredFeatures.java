3fb69c80c68c28cea9924b5293182870d2d9198b
Louwind <46799905+Louwind@users.noreply.github.com>
2020-10-03 19:50:56 -0700

    Add: Trunks and Basements

--- src/main/java/github/KingVampyre/DeepTrenches/core/init/ModConfiguredFeatures.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/init/ModConfiguredFeatures.java
@@ -8,6 +8,8 @@ public class ModConfiguredFeatures {
 
     public static final ConfiguredFeature<?, ?> ANAMEATA = new ConfiguredFeature<>(ModFeatures.ANAMEATA, INSTANCE);
 
+    public static final ConfiguredFeature<?, ?> ANAMEATA_BASEMENT = new ConfiguredFeature<>(ModFeatures.ANAMEATA_BASEMENT, INSTANCE);
+
     public static final ConfiguredFeature<?, ?> BARSHROOKLE = new ConfiguredFeature<>(ModFeatures.BARSHROOKLE, INSTANCE);
 
     public static final ConfiguredFeature<?, ?> BIG_ALMOND = new ConfiguredFeature<>(ModFeatures.BIG_ALMOND, INSTANCE);
@@ -20,7 +22,13 @@ public class ModConfiguredFeatures {
 
     public static final ConfiguredFeature<?, ?> BIG_BLACK_BIRCH = new ConfiguredFeature<>(ModFeatures.BIG_BLACK_BIRCH, INSTANCE);
 
+    public static final ConfiguredFeature<?, ?> BIG_BLACK_BIRCH_TRUNK = new ConfiguredFeature<>(ModFeatures.BIG_BLACK_BIRCH_TRUNK, INSTANCE);
+
     public static final ConfiguredFeature<?, ?> BIG_FUCHSITRA = new ConfiguredFeature<>(ModFeatures.BIG_FUCHSITRA, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> BIG_FUCHSITRA_BASEMENT = new ConfiguredFeature<>(ModFeatures.BIG_FUCHSITRA_BASEMENT, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> BIG_FUCHSITRA_TRUNK = new ConfiguredFeature<>(ModFeatures.BIG_FUCHSITRA_TRUNK, INSTANCE);
 
     public static final ConfiguredFeature<?, ?> BIG_PLUM = new ConfiguredFeature<>(ModFeatures.BIG_PLUM, INSTANCE);
 
@@ -32,6 +40,8 @@ public class ModConfiguredFeatures {
 
     public static final ConfiguredFeature<?, ?> CROLOOD = new ConfiguredFeature<>(ModFeatures.CROLOOD, INSTANCE);
 
+    public static final ConfiguredFeature<?, ?> CROLOOD_BASEMENT = new ConfiguredFeature<>(ModFeatures.CROLOOD_BASEMENT, INSTANCE);
+
     public static final ConfiguredFeature<?, ?> DAMP_BARSHROOKLE = new ConfiguredFeature<>(ModFeatures.DAMP_BARSHROOKLE, INSTANCE);
 
     public static final ConfiguredFeature<?, ?> DAMP_GHOSHROOM = new ConfiguredFeature<>(ModFeatures.DAMP_GHOSHROOM, INSTANCE);
@@ -40,11 +50,21 @@ public class ModConfiguredFeatures {
 
     public static final ConfiguredFeature<?, ?> DARK_CROLOOD = new ConfiguredFeature<>(ModFeatures.DARK_CROLOOD, INSTANCE);
 
+    public static final ConfiguredFeature<?, ?> DARK_CROLOOD_BASEMENT = new ConfiguredFeature<>(ModFeatures.DARK_CROLOOD_BASEMENT, INSTANCE);
+
     public static final ConfiguredFeature<?, ?> EBONY = new ConfiguredFeature<>(ModFeatures.EBONY, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> FUNERANITE = new ConfiguredFeature<>(ModFeatures.FUNERANITE, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> FUNERANITE_BASEMENT = new ConfiguredFeature<>(ModFeatures.FUNERANITE_BASEMENT, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> FUNERANITE_TRUNK = new ConfiguredFeature<>(ModFeatures.FUNERANITE_TRUNK, INSTANCE);
 
     public static final ConfiguredFeature<?, ?> GHOSHROOM = new ConfiguredFeature<>(ModFeatures.GHOSHROOM, INSTANCE);
 
-    public static final ConfiguredFeature<?, ?> FUNERANITE = new ConfiguredFeature<>(ModFeatures.FUNERANITE, INSTANCE);
+    public static final ConfiguredFeature<?, ?> GHOSHROOM_BASEMENT = new ConfiguredFeature<>(ModFeatures.GHOSHROOM_BASEMENT, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> GHOSHROOM_TRUNK = new ConfiguredFeature<>(ModFeatures.GHOSHROOM_TRUNK, INSTANCE);
 
     public static final ConfiguredFeature<?, ?> MEDIUM_ALMOND = new ConfiguredFeature<>(ModFeatures.MEDIUM_ALMOND, INSTANCE);
 
@@ -64,13 +84,17 @@ public class ModConfiguredFeatures {
 
     public static final ConfiguredFeature<?, ?> PELTOGYNE = new ConfiguredFeature<>(ModFeatures.PELTOGYNE, INSTANCE);
 
+    public static final ConfiguredFeature<?, ?> PELTOGYNE_TRUNK = new ConfiguredFeature<>(ModFeatures.PELTOGYNE_TRUNK, INSTANCE);
+
     public static final ConfiguredFeature<?, ?> PURFUNGA = new ConfiguredFeature<>(ModFeatures.PURFUNGA, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> PURFUNGA_BASEMENT = new ConfiguredFeature<>(ModFeatures.PURFUNGA_BASEMENT, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> PURFUNGA_TRUNK = new ConfiguredFeature<>(ModFeatures.PURFUNGA_TRUNK, INSTANCE);
 
     public static final ConfiguredFeature<?, ?> SPROOM = new ConfiguredFeature<>(ModFeatures.SPROOM, INSTANCE);
 
-    public static final ConfiguredFeature<?, ?> STORTREEAN = new ConfiguredFeature<>(ModFeatures.STORTREEAN, INSTANCE);
-
-    public static final ConfiguredFeature<?, ?> SUNRISE_FUNGUS = new ConfiguredFeature<>(ModFeatures.SUNRISE_FUNGUS, INSTANCE);
+    public static final ConfiguredFeature<?, ?> SPROOM_TRUNK = new ConfiguredFeature<>(ModFeatures.SPROOM_TRUNK, INSTANCE);
 
     public static final ConfiguredFeature<?, ?> SMALL_ALMOND = new ConfiguredFeature<>(ModFeatures.SMALL_ALMOND, INSTANCE);
 
@@ -91,5 +115,19 @@ public class ModConfiguredFeatures {
     public static final ConfiguredFeature<?, ?> SMALL_THUNDERCLOUD_PLUM = new ConfiguredFeature<>(ModFeatures.SMALL_THUNDERCLOUD_PLUM, INSTANCE);
 
     public static final ConfiguredFeature<?, ?> STROOMEAN = new ConfiguredFeature<>(ModFeatures.STROOMEAN, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> STROOMEAN_BASEMENT = new ConfiguredFeature<>(ModFeatures.STROOMEAN_BASEMENT, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> STROOMEAN_TRUNK = new ConfiguredFeature<>(ModFeatures.STROOMEAN_TRUNK, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> STORTREEAN = new ConfiguredFeature<>(ModFeatures.STORTREEAN, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> STORTREEAN_TRUNK = new ConfiguredFeature<>(ModFeatures.STORTREEAN_TRUNK, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> SUNRISE_FUNGUS = new ConfiguredFeature<>(ModFeatures.SUNRISE_FUNGUS, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> SUNRISE_FUNGUS_BASEMENT = new ConfiguredFeature<>(ModFeatures.SUNRISE_FUNGUS_BASEMENT, INSTANCE);
+
+    public static final ConfiguredFeature<?, ?> TEAK_TRUNK = new ConfiguredFeature<>(ModFeatures.TEAK_TRUNK, INSTANCE);
 
 }
--- src/main/java/github/KingVampyre/DeepTrenches/core/init/ModFeatures.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/init/ModFeatures.java
@@ -8,6 +8,8 @@ public class ModFeatures {
 
     public static final GenericFeature<DefaultFeatureConfig> ANAMEATA = new GenericFeature<>(new Identifier("deep_trenches:anameata"), DefaultFeatureConfig.CODEC);
 
+    public static final GenericFeature<DefaultFeatureConfig> ANAMEATA_BASEMENT = new GenericFeature<>(new Identifier("deep_trenches:anameata_basement"), DefaultFeatureConfig.CODEC);
+
     public static final GenericFeature<DefaultFeatureConfig> BARSHROOKLE = new GenericFeature<>(new Identifier("deep_trenches:barshrookle"), DefaultFeatureConfig.CODEC);
 
     public static final GenericFeature<DefaultFeatureConfig> BIG_ALMOND = new GenericFeature<>(new Identifier("deep_trenches:big_almond"), DefaultFeatureConfig.CODEC);
@@ -20,7 +22,13 @@ public class ModFeatures {
 
     public static final GenericFeature<DefaultFeatureConfig> BIG_BLACK_BIRCH = new GenericFeature<>(new Identifier("deep_trenches:big_black_birch"), DefaultFeatureConfig.CODEC);
 
+    public static final GenericFeature<DefaultFeatureConfig> BIG_BLACK_BIRCH_TRUNK = new GenericFeature<>(new Identifier("deep_trenches:big_black_birch_trunk"), DefaultFeatureConfig.CODEC);
+
     public static final GenericFeature<DefaultFeatureConfig> BIG_FUCHSITRA = new GenericFeature<>(new Identifier("deep_trenches:big_fuchsitra"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> BIG_FUCHSITRA_BASEMENT = new GenericFeature<>(new Identifier("deep_trenches:big_fuchsitra_basement"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> BIG_FUCHSITRA_TRUNK = new GenericFeature<>(new Identifier("deep_trenches:big_fuchsitra_trunk"), DefaultFeatureConfig.CODEC);
 
     public static final GenericFeature<DefaultFeatureConfig> BIG_PLUM = new GenericFeature<>(new Identifier("deep_trenches:big_plum"), DefaultFeatureConfig.CODEC);
 
@@ -32,6 +40,8 @@ public class ModFeatures {
 
     public static final GenericFeature<DefaultFeatureConfig> CROLOOD = new GenericFeature<>(new Identifier("deep_trenches:crolood"), DefaultFeatureConfig.CODEC);
 
+    public static final GenericFeature<DefaultFeatureConfig> CROLOOD_BASEMENT = new GenericFeature<>(new Identifier("deep_trenches:crolood_basement"), DefaultFeatureConfig.CODEC);
+
     public static final GenericFeature<DefaultFeatureConfig> DAMP_BARSHROOKLE = new GenericFeature<>(new Identifier("deep_trenches:damp_barshrookle"), DefaultFeatureConfig.CODEC);
 
     public static final GenericFeature<DefaultFeatureConfig> DAMP_GHOSHROOM = new GenericFeature<>(new Identifier("deep_trenches:damp_ghoshroom"), DefaultFeatureConfig.CODEC);
@@ -40,11 +50,21 @@ public class ModFeatures {
 
     public static final GenericFeature<DefaultFeatureConfig> DARK_CROLOOD = new GenericFeature<>(new Identifier("deep_trenches:dark_crolood"), DefaultFeatureConfig.CODEC);
 
+    public static final GenericFeature<DefaultFeatureConfig> DARK_CROLOOD_BASEMENT = new GenericFeature<>(new Identifier("deep_trenches:dark_crolood_basement"), DefaultFeatureConfig.CODEC);
+
     public static final GenericFeature<DefaultFeatureConfig> EBONY = new GenericFeature<>(new Identifier("deep_trenches:ebony"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> FUNERANITE = new GenericFeature<>(new Identifier("deep_trenches:funeranite"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> FUNERANITE_BASEMENT = new GenericFeature<>(new Identifier("deep_trenches:funeranite_basement"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> FUNERANITE_TRUNK = new GenericFeature<>(new Identifier("deep_trenches:funeranite_trunk"), DefaultFeatureConfig.CODEC);
 
     public static final GenericFeature<DefaultFeatureConfig> GHOSHROOM = new GenericFeature<>(new Identifier("deep_trenches:ghoshroom"), DefaultFeatureConfig.CODEC);
 
-    public static final GenericFeature<DefaultFeatureConfig> FUNERANITE = new GenericFeature<>(new Identifier("deep_trenches:funeranite"), DefaultFeatureConfig.CODEC);
+    public static final GenericFeature<DefaultFeatureConfig> GHOSHROOM_BASEMENT = new GenericFeature<>(new Identifier("deep_trenches:ghoshroom_basement"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> GHOSHROOM_TRUNK = new GenericFeature<>(new Identifier("deep_trenches:ghoshroom_trunk"), DefaultFeatureConfig.CODEC);
 
     public static final GenericFeature<DefaultFeatureConfig> MEDIUM_ALMOND = new GenericFeature<>(new Identifier("deep_trenches:medium_almond"), DefaultFeatureConfig.CODEC);
 
@@ -64,13 +84,17 @@ public class ModFeatures {
 
     public static final GenericFeature<DefaultFeatureConfig> PELTOGYNE = new GenericFeature<>(new Identifier("deep_trenches:peltogyne"), DefaultFeatureConfig.CODEC);
 
+    public static final GenericFeature<DefaultFeatureConfig> PELTOGYNE_TRUNK = new GenericFeature<>(new Identifier("deep_trenches:peltogyne_trunk"), DefaultFeatureConfig.CODEC);
+
     public static final GenericFeature<DefaultFeatureConfig> PURFUNGA = new GenericFeature<>(new Identifier("deep_trenches:purfunga"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> PURFUNGA_BASEMENT = new GenericFeature<>(new Identifier("deep_trenches:purfunga_basement"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> PURFUNGA_TRUNK = new GenericFeature<>(new Identifier("deep_trenches:purfunga_trunk"), DefaultFeatureConfig.CODEC);
 
     public static final GenericFeature<DefaultFeatureConfig> SPROOM = new GenericFeature<>(new Identifier("deep_trenches:sproom"), DefaultFeatureConfig.CODEC);
 
-    public static final GenericFeature<DefaultFeatureConfig> STORTREEAN = new GenericFeature<>(new Identifier("deep_trenches:stortreean"), DefaultFeatureConfig.CODEC);
-
-    public static final GenericFeature<DefaultFeatureConfig> SUNRISE_FUNGUS = new GenericFeature<>(new Identifier("deep_trenches:sunrise_fungus"), DefaultFeatureConfig.CODEC);
+    public static final GenericFeature<DefaultFeatureConfig> SPROOM_TRUNK = new GenericFeature<>(new Identifier("deep_trenches:sproom_trunk"), DefaultFeatureConfig.CODEC);
 
     public static final GenericFeature<DefaultFeatureConfig> SMALL_ALMOND = new GenericFeature<>(new Identifier("deep_trenches:small_almond"), DefaultFeatureConfig.CODEC);
 
@@ -91,5 +115,19 @@ public class ModFeatures {
     public static final GenericFeature<DefaultFeatureConfig> SMALL_THUNDERCLOUD_PLUM = new GenericFeature<>(new Identifier("deep_trenches:small_thundercloud_plum"), DefaultFeatureConfig.CODEC);
 
     public static final GenericFeature<DefaultFeatureConfig> STROOMEAN = new GenericFeature<>(new Identifier("deep_trenches:stroomean"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> STROOMEAN_BASEMENT = new GenericFeature<>(new Identifier("deep_trenches:stroomean_basement"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> STROOMEAN_TRUNK = new GenericFeature<>(new Identifier("deep_trenches:stroomean_trunk"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> STORTREEAN = new GenericFeature<>(new Identifier("deep_trenches:stortreean"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> STORTREEAN_TRUNK = new GenericFeature<>(new Identifier("deep_trenches:stortreean_trunk"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> SUNRISE_FUNGUS = new GenericFeature<>(new Identifier("deep_trenches:sunrise_fungus"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> SUNRISE_FUNGUS_BASEMENT = new GenericFeature<>(new Identifier("deep_trenches:sunrise_fungus_basement"), DefaultFeatureConfig.CODEC);
+
+    public static final GenericFeature<DefaultFeatureConfig> TEAK_TRUNK = new GenericFeature<>(new Identifier("deep_trenches:teak_trunk"), DefaultFeatureConfig.CODEC);
 
 }
