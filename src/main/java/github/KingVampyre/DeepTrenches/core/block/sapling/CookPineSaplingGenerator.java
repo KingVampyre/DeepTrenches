b8e6d0d110d281376df9946a42064819d27d7b31
Louwind <46799905+Louwind@users.noreply.github.com>
2020-11-10 15:03:27 -0800

    Fix: Cook Pine offsets and rotations!

--- src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/CookPineSaplingGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/CookPineSaplingGenerator.java
@@ -1,7 +1,9 @@
 package github.KingVampyre.DeepTrenches.core.block.sapling;
 
+import com.google.common.collect.ImmutableMap;
 import github.Louwind.Features.impl.block.sapling.FeaturesThickSaplingGenerator;
 import net.minecraft.block.Block;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
@@ -10,13 +12,23 @@ import java.util.Random;
 import static github.KingVampyre.DeepTrenches.core.init.ModBlocks.COOK_PINE_SAPLING;
 import static net.minecraft.util.registry.BuiltinRegistries.CONFIGURED_FEATURE;
 
+import static net.minecraft.util.BlockRotation.*;
+
 public class CookPineSaplingGenerator implements FeaturesThickSaplingGenerator {
 
-    private static final Identifier COOK_PINE = new Identifier("deep_trenches:cook_pine");
+    private static final ImmutableMap<BlockRotation, Identifier> FEATURES = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:cook_pine"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:cook_pine_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:cook_pine_180"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:cook_pine_270"))
+            .build();
 
     @Override
     public ConfiguredFeature<?, ?> createThickTreeFeature(Random random, boolean sapling) {
-        return CONFIGURED_FEATURE.get(COOK_PINE);
+        BlockRotation rotation = BlockRotation.random(random);
+        Identifier feature = FEATURES.get(rotation);
+
+        return CONFIGURED_FEATURE.get(feature);
     }
 
     @Override
