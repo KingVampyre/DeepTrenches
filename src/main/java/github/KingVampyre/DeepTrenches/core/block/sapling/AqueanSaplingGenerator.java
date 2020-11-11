9be59955943e13b0bb5636ce7e7a1cbd75f3fc61
Louwind <46799905+Louwind@users.noreply.github.com>
2020-11-11 14:12:52 -0800

    Fix: Add rotations to Aquean
    Fix: Aquean pivot when rotation is 270 deg.
    Fix: Aquean heights

--- src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/AqueanSaplingGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/AqueanSaplingGenerator.java
@@ -1,29 +1,59 @@
 package github.KingVampyre.DeepTrenches.core.block.sapling;
 
+import com.google.common.collect.ImmutableMap;
 import github.Louwind.Features.impl.block.sapling.FeaturesSaplingGenerator;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 import java.util.Random;
 
+import static net.minecraft.util.BlockRotation.*;
+import static net.minecraft.util.BlockRotation.COUNTERCLOCKWISE_90;
 import static net.minecraft.util.registry.BuiltinRegistries.CONFIGURED_FEATURE;
 
 public class AqueanSaplingGenerator implements FeaturesSaplingGenerator {
 
-    private static final Identifier BIG_AQUEAN = new Identifier("deep_trenches:big_aquean");
-    private static final Identifier MEDIUM_AQUEAN = new Identifier("deep_trenches:medium_aquean");
-    private static final Identifier SMALL_AQUEAN = new Identifier("deep_trenches:small_aquean");
+    private static final ImmutableMap<BlockRotation, Identifier> BIG_AQUEANS = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:big_aquean"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:big_aquean_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:big_aquean_180"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:big_aquean_270"))
+            .build();
+
+    private static final ImmutableMap<BlockRotation, Identifier> MEDIUM_AQUEANS = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:medium_aquean"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:medium_aquean_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:medium_aquean_180"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:medium_aquean_270"))
+            .build();
+
+    private static final ImmutableMap<BlockRotation, Identifier> SMALL_AQUEANS = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:small_aquean"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:small_aquean_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:small_aquean_180"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:small_aquean_270"))
+            .build();
 
     @Override
     public ConfiguredFeature<?, ?> createTreeFeature(Random random, boolean sapling) {
+        BlockRotation rotation = BlockRotation.random(random);
 
-        if (random.nextInt(7) == 0)
-            return CONFIGURED_FEATURE.get(BIG_AQUEAN);
+        if (random.nextInt(7) == 0) {
+            Identifier id = BIG_AQUEANS.get(rotation);
 
-        if (random.nextInt(3) == 0)
-            return CONFIGURED_FEATURE.get(MEDIUM_AQUEAN);
+            return CONFIGURED_FEATURE.get(id);
+        }
 
-        return CONFIGURED_FEATURE.get(SMALL_AQUEAN);
+        if (random.nextInt(3) == 0) {
+            Identifier id = MEDIUM_AQUEANS.get(rotation);
+
+            return CONFIGURED_FEATURE.get(id);
+        }
+
+        Identifier id = SMALL_AQUEANS.get(rotation);
+
+        return CONFIGURED_FEATURE.get(id);
     }
 
 }
