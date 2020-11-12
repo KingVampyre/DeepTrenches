116ba8791e0f997767430619d6604bcdf94da060
Louwind <46799905+Louwind@users.noreply.github.com>
2020-11-11 18:24:54 -0800

    Fix: Fuchsitra offsets & rotations
    Note: Update Features API to fix the Addition & Multiply getters for Big Fuchsitra.

--- src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/FuchsitraSaplingGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/FuchsitraSaplingGenerator.java
@@ -1,24 +1,55 @@
 package github.KingVampyre.DeepTrenches.core.block.sapling;
 
+import com.google.common.collect.ImmutableMap;
 import github.Louwind.Features.impl.block.sapling.FeaturesThickSaplingGenerator;
 import net.minecraft.block.Block;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 import java.util.Random;
 
 import static github.KingVampyre.DeepTrenches.core.init.ModBlocks.FUCHSITRA_SAPLING;
+import static net.minecraft.util.BlockRotation.*;
+import static net.minecraft.util.BlockRotation.COUNTERCLOCKWISE_90;
 import static net.minecraft.util.registry.BuiltinRegistries.CONFIGURED_FEATURE;
 
 public class FuchsitraSaplingGenerator implements FeaturesThickSaplingGenerator {
 
-    private static final Identifier BIG_FUCHSITRA = new Identifier("deep_trenches:big_fuchsitra");
-    private static final Identifier MEDIUM_FUCHSITRA = new Identifier("deep_trenches:medium_fuchsitra");
-    private static final Identifier SMALL_FUCHSITRA = new Identifier("deep_trenches:small_fuchsitra");
+    private static final ImmutableMap<BlockRotation, Identifier> BIG_FUCHSITRAS = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:big_fuchsitra"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:big_fuchsitra_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:big_fuchsitra"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:big_fuchsitra_90"))
+            .build();
+
+    private static final ImmutableMap<BlockRotation, Identifier> MEDIUM_FUCHSITRAS = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:medium_fuchsitra"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:medium_fuchsitra_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:medium_fuchsitra"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:medium_fuchsitra_90"))
+            .build();
+
+    private static final ImmutableMap<BlockRotation, Identifier> SMALL_FUCHSITRAS = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:small_fuchsitra"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:small_fuchsitra_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:small_fuchsitra"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:small_fuchsitra_90"))
+            .build();
 
     @Override
     public ConfiguredFeature<?, ?> createThickTreeFeature(Random random, boolean sapling) {
-        return random.nextInt(5) == 0 ? CONFIGURED_FEATURE.get(BIG_FUCHSITRA) : CONFIGURED_FEATURE.get(MEDIUM_FUCHSITRA);
+        BlockRotation rotation = BlockRotation.random(random);
+
+        if(random.nextInt(5) == 0) {
+            Identifier id = BIG_FUCHSITRAS.get(rotation);
+
+            return CONFIGURED_FEATURE.get(id);
+        }
+
+        Identifier id = MEDIUM_FUCHSITRAS.get(rotation);
+
+        return CONFIGURED_FEATURE.get(id);
     }
 
     @Override
@@ -28,7 +59,10 @@ public class FuchsitraSaplingGenerator implements FeaturesThickSaplingGenerator 
 
     @Override
     public ConfiguredFeature<?, ?> createTreeFeature(Random random, boolean sapling) {
-        return CONFIGURED_FEATURE.get(SMALL_FUCHSITRA);
+        BlockRotation rotation = BlockRotation.random(random);
+        Identifier id = SMALL_FUCHSITRAS.get(rotation);
+
+        return CONFIGURED_FEATURE.get(id);
     }
 
 }
