182a0bf2cb06ac363ba8c8610b7c6b141b02804e
Louwind <46799905+Louwind@users.noreply.github.com>
2020-11-12 17:38:42 -0800

    Fix: Stortreean Tree

--- src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/StortreeanSaplingGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/StortreeanSaplingGenerator.java
@@ -1,22 +1,34 @@
 package github.KingVampyre.DeepTrenches.core.block.sapling;
 
+import com.google.common.collect.ImmutableMap;
 import github.Louwind.Features.impl.block.sapling.FeaturesThickSaplingGenerator;
 import net.minecraft.block.Block;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 import java.util.Random;
 
 import static github.KingVampyre.DeepTrenches.core.init.ModBlocks.STORTREEAN_SAPLING;
+import static net.minecraft.util.BlockRotation.*;
+import static net.minecraft.util.BlockRotation.COUNTERCLOCKWISE_90;
 import static net.minecraft.util.registry.BuiltinRegistries.CONFIGURED_FEATURE;
 
 public class StortreeanSaplingGenerator implements FeaturesThickSaplingGenerator {
 
-    private static final Identifier STORTREEAN = new Identifier("deep_trenches:stortreean");
+    private static final ImmutableMap<BlockRotation, Identifier> STORTREEANS = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:stortreean"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:stortreean_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:stortreean"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:stortreean_90"))
+            .build();
 
     @Override
     public ConfiguredFeature<?, ?> createThickTreeFeature(Random random, boolean sapling) {
-        return CONFIGURED_FEATURE.get(STORTREEAN);
+        BlockRotation rotation = BlockRotation.random(random);
+        Identifier id = STORTREEANS.get(rotation);
+
+        return CONFIGURED_FEATURE.get(id);
     }
 
     @Override
