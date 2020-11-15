a81f5a1dea41bf0fc15395770d3c21360c80845a
Louwind <46799905+Louwind@users.noreply.github.com>
2020-11-15 10:53:37 -0800

    Fix: Dark Crolood not generated properly

--- src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/DarkCroloodSaplingGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/DarkCroloodSaplingGenerator.java
@@ -1,22 +1,33 @@
 package github.KingVampyre.DeepTrenches.core.block.sapling;
 
+import com.google.common.collect.ImmutableMap;
 import github.Louwind.Features.impl.block.sapling.FeaturesThickSaplingGenerator;
 import net.minecraft.block.Block;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 import java.util.Random;
 
 import static github.KingVampyre.DeepTrenches.core.init.ModBlocks.DARK_CROLOOD_SAPLING;
+import static net.minecraft.util.BlockRotation.*;
 import static net.minecraft.util.registry.BuiltinRegistries.CONFIGURED_FEATURE;
 
 public class DarkCroloodSaplingGenerator implements FeaturesThickSaplingGenerator {
 
-    private static final Identifier DARK_CROLOOD = new Identifier("deep_trenches:dark_crolood");
+    private static final ImmutableMap<BlockRotation, Identifier> DARK_CROLOOD = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:dark_crolood"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:dark_crolood_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:dark_crolood"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:dark_crolood_90"))
+            .build();
 
     @Override
     public ConfiguredFeature<?, ?> createThickTreeFeature(Random random, boolean sapling) {
-        return CONFIGURED_FEATURE.get(DARK_CROLOOD);
+        BlockRotation rotation = BlockRotation.random(random);
+        Identifier id = DARK_CROLOOD.get(rotation);
+
+        return CONFIGURED_FEATURE.get(id);
     }
 
     @Override
