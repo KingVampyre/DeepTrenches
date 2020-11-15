572e0e815825d8d49982ae33143ac89afc844c04
Louwind <46799905+Louwind@users.noreply.github.com>
2020-11-15 10:55:44 -0800

    Fix: Peltogyne not generated properly

--- src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/PeltogyneSaplingGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/PeltogyneSaplingGenerator.java
@@ -1,22 +1,33 @@
 package github.KingVampyre.DeepTrenches.core.block.sapling;
 
+import com.google.common.collect.ImmutableMap;
 import github.Louwind.Features.impl.block.sapling.FeaturesThickSaplingGenerator;
 import net.minecraft.block.Block;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 import java.util.Random;
 
 import static github.KingVampyre.DeepTrenches.core.init.ModBlocks.PELTOGYNE_SAPLING;
+import static net.minecraft.util.BlockRotation.*;
 import static net.minecraft.util.registry.BuiltinRegistries.CONFIGURED_FEATURE;
 
 public class PeltogyneSaplingGenerator implements FeaturesThickSaplingGenerator {
 
-    private static final Identifier PELTOGYNE = new Identifier("deep_trenches:peltogyne");
+    private static final ImmutableMap<BlockRotation, Identifier> PELTOGYNE = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:peltogyne"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:peltogyne_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:peltogyne_180"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:peltogyne_270"))
+            .build();
 
     @Override
     public ConfiguredFeature<?, ?> createThickTreeFeature(Random random, boolean sapling) {
-        return CONFIGURED_FEATURE.get(PELTOGYNE);
+        BlockRotation rotation = BlockRotation.random(random);
+        Identifier id = PELTOGYNE.get(rotation);
+
+        return CONFIGURED_FEATURE.get(id);
     }
 
     @Override
