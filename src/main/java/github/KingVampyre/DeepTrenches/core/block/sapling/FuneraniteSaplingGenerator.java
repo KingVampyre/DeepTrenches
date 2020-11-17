1cf989edf7529585dbba136076193e60611715c9
Louwind <46799905+Louwind@users.noreply.github.com>
2020-11-17 12:07:14 -0800

    Fix: Funeranite fungus not generated properly

--- src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/FuneraniteSaplingGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/FuneraniteSaplingGenerator.java
@@ -1,22 +1,33 @@
 package github.KingVampyre.DeepTrenches.core.block.sapling;
 
+import com.google.common.collect.ImmutableMap;
 import github.Louwind.Features.impl.block.sapling.FeaturesThickSaplingGenerator;
 import net.minecraft.block.Block;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 import java.util.Random;
 
 import static github.KingVampyre.DeepTrenches.core.init.ModBlocks.FUNERANITE_SAPLING;
+import static net.minecraft.util.BlockRotation.*;
 import static net.minecraft.util.registry.BuiltinRegistries.CONFIGURED_FEATURE;
 
 public class FuneraniteSaplingGenerator implements FeaturesThickSaplingGenerator {
 
-    private static final Identifier FUNERANITE = new Identifier("deep_trenches:funeranite");
+    private static final ImmutableMap<BlockRotation, Identifier> FUNERANITE = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:funeranite"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:funeranite_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:funeranite_180"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:funeranite_270"))
+            .build();
 
     @Override
     public ConfiguredFeature<?, ?> createThickTreeFeature(Random random, boolean sapling) {
-        return CONFIGURED_FEATURE.get(FUNERANITE);
+        BlockRotation rotation = BlockRotation.random(random);
+        Identifier id = FUNERANITE.get(rotation);
+
+        return CONFIGURED_FEATURE.get(id);
     }
 
     @Override
