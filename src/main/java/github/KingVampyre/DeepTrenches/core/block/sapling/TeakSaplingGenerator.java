ff359a3b860d45fa2c132a60e4a7da6c4cd0fd49
Louwind <46799905+Louwind@users.noreply.github.com>
2020-11-16 16:11:50 -0800

    Fix: Teak trees not generated properly

--- src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/TeakSaplingGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/TeakSaplingGenerator.java
@@ -1,24 +1,41 @@
 package github.KingVampyre.DeepTrenches.core.block.sapling;
 
+import com.google.common.collect.ImmutableMap;
 import github.Louwind.Features.impl.block.sapling.FeaturesThickSaplingGenerator;
 import net.minecraft.block.Block;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 import java.util.Random;
 
 import static github.KingVampyre.DeepTrenches.core.init.ModBlocks.TEAK_SAPLING;
+import static net.minecraft.util.BlockRotation.*;
 import static net.minecraft.util.registry.BuiltinRegistries.CONFIGURED_FEATURE;
 
 public class TeakSaplingGenerator implements FeaturesThickSaplingGenerator {
 
     private static final Identifier BIG_TEAK = new Identifier("deep_trenches:big_teak");
-    private static final Identifier MEDIUM_TEAK = new Identifier("deep_trenches:medium_teak");
+
+    private static final ImmutableMap<BlockRotation, Identifier> MEDIUM_TEAK = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:medium_teak"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:medium_teak_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:medium_teak"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:medium_teak_90"))
+            .build();
+
     private static final Identifier SMALL_TEAK = new Identifier("deep_trenches:small_teak");
 
     @Override
     public ConfiguredFeature<?, ?> createThickTreeFeature(Random random, boolean sapling) {
-        return random.nextInt(10) == 0 ? CONFIGURED_FEATURE.get(BIG_TEAK) : CONFIGURED_FEATURE.get(MEDIUM_TEAK);
+
+        if(random.nextInt(10) == 0)
+            return CONFIGURED_FEATURE.get(BIG_TEAK);
+
+        BlockRotation rotation = BlockRotation.random(random);
+        Identifier id = MEDIUM_TEAK.get(rotation);
+
+        return CONFIGURED_FEATURE.get(id);
     }
 
     @Override
