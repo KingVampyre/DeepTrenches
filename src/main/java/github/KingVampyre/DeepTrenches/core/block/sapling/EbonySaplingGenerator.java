5ab5acc79ec07ff13b1eed4a0f4fe8728472940b
Louwind <46799905+Louwind@users.noreply.github.com>
2020-11-16 16:09:05 -0800

    Fix: Ebony tree not generated properly

--- src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/EbonySaplingGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/EbonySaplingGenerator.java
@@ -1,20 +1,31 @@
 package github.KingVampyre.DeepTrenches.core.block.sapling;
 
+import com.google.common.collect.ImmutableMap;
 import github.Louwind.Features.impl.block.sapling.FeaturesSaplingGenerator;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 import java.util.Random;
 
+import static net.minecraft.util.BlockRotation.*;
 import static net.minecraft.util.registry.BuiltinRegistries.CONFIGURED_FEATURE;
 
 public class EbonySaplingGenerator implements FeaturesSaplingGenerator {
 
-    private static final Identifier FUNERANITE = new Identifier("deep_trenches:funeranite");
+    private static final ImmutableMap<BlockRotation, Identifier> EBONY = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:ebony"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:ebony_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:ebony"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:ebony_90"))
+            .build();
 
     @Override
     public ConfiguredFeature<?, ?> createTreeFeature(Random random, boolean sapling) {
-        return CONFIGURED_FEATURE.get(FUNERANITE);
+        BlockRotation rotation = BlockRotation.random(random);
+        Identifier id = EBONY.get(rotation);
+
+        return CONFIGURED_FEATURE.get(id);
     }
 
 }
