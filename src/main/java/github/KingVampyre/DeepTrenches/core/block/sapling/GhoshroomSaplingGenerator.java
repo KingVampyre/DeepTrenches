c99db0d77e9e6a5e7c573c0237be044e28859db1
Louwind <46799905+Louwind@users.noreply.github.com>
2020-11-17 13:22:49 -0800

    Fix: Ghoshroom fungus not generated properly

--- src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/GhoshroomSaplingGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/GhoshroomSaplingGenerator.java
@@ -1,23 +1,33 @@
 package github.KingVampyre.DeepTrenches.core.block.sapling;
 
+import com.google.common.collect.ImmutableMap;
 import github.Louwind.Features.impl.block.sapling.FeaturesThickSaplingGenerator;
 import net.minecraft.block.Block;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 import java.util.Random;
 
 import static github.KingVampyre.DeepTrenches.core.init.ModBlocks.GHOSHROOM_SAPLING;
+import static net.minecraft.util.BlockRotation.*;
 import static net.minecraft.util.registry.BuiltinRegistries.CONFIGURED_FEATURE;
 
 public class GhoshroomSaplingGenerator implements FeaturesThickSaplingGenerator {
 
-    private static final Identifier GHOSHROOM = new Identifier("deep_trenches:ghoshroom");
-    private static final Identifier DAMP_GHOSHROOM = new Identifier("deep_trenches:damp_ghoshroom");
-
+    private static final ImmutableMap<BlockRotation, Identifier> GHOSHROOM = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:ghoshroom"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:ghoshroom_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:ghoshroom_180"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:ghoshroom_270"))
+            .build();
+    
     @Override
     public ConfiguredFeature<?, ?> createThickTreeFeature(Random random, boolean sapling) {
-        return sapling ? CONFIGURED_FEATURE.get(GHOSHROOM) : CONFIGURED_FEATURE.get(DAMP_GHOSHROOM);
+        BlockRotation rotation = BlockRotation.random(random);
+        Identifier id = GHOSHROOM.get(rotation);
+
+        return CONFIGURED_FEATURE.get(id);
     }
 
     @Override
