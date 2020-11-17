8264a317baca7fc8d3b049c0099607ab0bb3c69d
Louwind <46799905+Louwind@users.noreply.github.com>
2020-11-16 18:11:25 -0800

    Fix: Big & medium Pin Cherry not generated properly

--- src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/PinCherrySaplingGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/PinCherrySaplingGenerator.java
@@ -15,8 +15,19 @@ import static net.minecraft.util.registry.BuiltinRegistries.CONFIGURED_FEATURE;
 
 public class PinCherrySaplingGenerator implements FeaturesThickSaplingGenerator {
 
-    private static final Identifier BIG_PIN_CHERRY = new Identifier("deep_trenches:big_pin_cherry");
-    private static final Identifier MEDIUM_PIN_CHERRY = new Identifier("deep_trenches:medium_pin_cherry");
+    private static final ImmutableMap<BlockRotation, Identifier> BIG_PIN_CHERRY = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:big_pin_cherry"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:big_pin_cherry_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:big_pin_cherry_180"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:big_pin_cherry_270"))
+            .build();
+
+    private static final ImmutableMap<BlockRotation, Identifier> MEDIUM_PIN_CHERRY = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:medium_pin_cherry"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:medium_pin_cherry_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:medium_pin_cherry_180"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:medium_pin_cherry_270"))
+            .build();
 
     private static final ImmutableMap<BlockRotation, Identifier> SMALL_PIN_CHERRY = ImmutableMap.<BlockRotation, Identifier>builder()
             .put(NONE, new Identifier("deep_trenches:small_pin_cherry"))
@@ -27,15 +38,21 @@ public class PinCherrySaplingGenerator implements FeaturesThickSaplingGenerator 
 
     @Override
     public ConfiguredFeature<?, ?> createThickTreeFeature(Random random, boolean sapling) {
-        return CONFIGURED_FEATURE.get(BIG_PIN_CHERRY);
+        BlockRotation rotation = BlockRotation.random(random);
+        Identifier id = BIG_PIN_CHERRY.get(rotation);
+
+        return CONFIGURED_FEATURE.get(id);
     }
 
     @Override
     public ConfiguredFeature<?, ?> createTreeFeature(Random random, boolean sapling) {
         BlockRotation rotation = BlockRotation.random(random);
 
-        if(random.nextInt(5) == 0)
-            return CONFIGURED_FEATURE.get(MEDIUM_PIN_CHERRY);
+        if(random.nextInt(5) == 0){
+            Identifier id = MEDIUM_PIN_CHERRY.get(rotation);
+
+            return CONFIGURED_FEATURE.get(id);
+        }
 
         Identifier id = SMALL_PIN_CHERRY.get(rotation);
 
