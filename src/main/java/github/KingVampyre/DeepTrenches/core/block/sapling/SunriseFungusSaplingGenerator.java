a91ba9074dcfc6eff389a5513c9683902723aac5
Louwind <46799905+Louwind@users.noreply.github.com>
2020-11-17 19:12:03 -0800

    Fix: Sunrise Fungus not generated properly

--- src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/SunriseFungusSaplingGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/SunriseFungusSaplingGenerator.java
@@ -1,22 +1,33 @@
 package github.KingVampyre.DeepTrenches.core.block.sapling;
 
+import com.google.common.collect.ImmutableMap;
 import github.Louwind.Features.impl.block.sapling.FeaturesThickSaplingGenerator;
 import net.minecraft.block.Block;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 import java.util.Random;
 
 import static github.KingVampyre.DeepTrenches.core.init.ModBlocks.SUNRISE_FUNGUS_SAPLING;
+import static net.minecraft.util.BlockRotation.*;
 import static net.minecraft.util.registry.BuiltinRegistries.CONFIGURED_FEATURE;
 
 public class SunriseFungusSaplingGenerator implements FeaturesThickSaplingGenerator {
 
-    private static final Identifier SUNRISE_FUNGUS = new Identifier("deep_trenches:sunrise_fungus");
+    private static final ImmutableMap<BlockRotation, Identifier> SUNRISE_FUNGUS = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:sunrise_fungus"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:sunrise_fungus_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:sunrise_fungus_180"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:sunrise_fungus_270"))
+            .build();
 
     @Override
     public ConfiguredFeature<?, ?> createThickTreeFeature(Random random, boolean sapling) {
-        return CONFIGURED_FEATURE.get(SUNRISE_FUNGUS);
+        BlockRotation rotation = BlockRotation.random(random);
+        Identifier id = SUNRISE_FUNGUS.get(rotation);
+
+        return CONFIGURED_FEATURE.get(id);
     }
 
     @Override
