921e7637f69aef581143e00c2d2c96d5995aa167
Louwind <46799905+Louwind@users.noreply.github.com>
2020-11-17 18:08:45 -0800

    Update: Stroomean fungus

--- src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/StroomeanSaplingGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/block/sapling/StroomeanSaplingGenerator.java
@@ -1,23 +1,33 @@
 package github.KingVampyre.DeepTrenches.core.block.sapling;
 
+import com.google.common.collect.ImmutableMap;
 import github.Louwind.Features.impl.block.sapling.FeaturesThickSaplingGenerator;
 import net.minecraft.block.Block;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
 import java.util.Random;
 
 import static github.KingVampyre.DeepTrenches.core.init.ModBlocks.STROOMEAN_SAPLING;
+import static net.minecraft.util.BlockRotation.*;
 import static net.minecraft.util.registry.BuiltinRegistries.CONFIGURED_FEATURE;
 
 public class StroomeanSaplingGenerator implements FeaturesThickSaplingGenerator {
 
-    private static final Identifier STROOMEAN = new Identifier("deep_trenches:stroomean");
-    private static final Identifier DAMP_STROOMEAN = new Identifier("deep_trenches:damp_stroomean");
+    private static final ImmutableMap<BlockRotation, Identifier> STROOMEAN = ImmutableMap.<BlockRotation, Identifier>builder()
+            .put(NONE, new Identifier("deep_trenches:stroomean"))
+            .put(CLOCKWISE_90, new Identifier("deep_trenches:stroomean_90"))
+            .put(CLOCKWISE_180, new Identifier("deep_trenches:stroomean_180"))
+            .put(COUNTERCLOCKWISE_90, new Identifier("deep_trenches:stroomean_270"))
+            .build();
 
     @Override
     public ConfiguredFeature<?, ?> createThickTreeFeature(Random random, boolean sapling) {
-        return sapling ? CONFIGURED_FEATURE.get(STROOMEAN) : CONFIGURED_FEATURE.get(DAMP_STROOMEAN);
+        BlockRotation rotation = BlockRotation.random(random);
+        Identifier id = STROOMEAN.get(rotation);
+
+        return CONFIGURED_FEATURE.get(id);
     }
 
     @Override
