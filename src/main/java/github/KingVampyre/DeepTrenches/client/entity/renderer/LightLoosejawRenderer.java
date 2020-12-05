822962b01b1cad7a6cebb2cdfe35b74713aae643
Louwind <46799905+Louwind@users.noreply.github.com>
2020-12-04 19:43:56 -0800

    Refactor: Remove unused imports

--- src/main/java/github/KingVampyre/DeepTrenches/client/entity/renderer/LightLoosejawRenderer.java
+++ src/main/java/github/KingVampyre/DeepTrenches/client/entity/renderer/LightLoosejawRenderer.java
@@ -1,12 +1,9 @@
 package github.KingVampyre.DeepTrenches.client.entity.renderer;
 
 import com.mojang.blaze3d.platform.GlStateManager;
-
 import github.KingVampyre.DeepTrenches.client.entity.model.LightLoosejawModel;
-import github.KingVampyre.DeepTrenches.client.entity.model.LightLoosejawTransparentModel;
 import github.KingVampyre.DeepTrenches.common.entity.renderer.DragonfishRenderer;
 import github.KingVampyre.DeepTrenches.core.entity.LightLoosejawEntity;
-import github.KingVampyre.DeepTrenches.client.entity.model.DummyModel;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.util.math.MatrixStack;
 
