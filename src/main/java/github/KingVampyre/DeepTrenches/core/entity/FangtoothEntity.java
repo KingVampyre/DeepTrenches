2bad2eb6bc2e41ec108107578ac67d1431590379
Louwind <46799905+Louwind@users.noreply.github.com>
2020-12-11 14:56:45 -0800

    Add: Fangtooth Model, Animations & Textures

---
+++ src/main/java/github/KingVampyre/DeepTrenches/core/entity/FangtoothEntity.java
@@ -0,0 +1,20 @@
+package github.KingVampyre.DeepTrenches.core.entity;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.PathAwareEntity;
+import net.minecraft.world.World;
+import software.bernie.geckolib.entity.IAnimatedEntity;
+import software.bernie.geckolib.manager.EntityAnimationManager;
+
+public class FangtoothEntity extends PathAwareEntity implements IAnimatedEntity {
+
+    public FangtoothEntity(EntityType<? extends PathAwareEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Override
+    public EntityAnimationManager getAnimationManager() {
+        return null;
+    }
+
+}
