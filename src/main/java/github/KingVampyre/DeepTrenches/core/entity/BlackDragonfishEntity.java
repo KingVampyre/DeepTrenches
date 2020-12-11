dfee1d15a847e47b9c763bebe82c09969be36658
Louwind <46799905+Louwind@users.noreply.github.com>
2020-12-11 14:00:38 -0800

    Add: Black Dragonfish Model, Animations & Textures

---
+++ src/main/java/github/KingVampyre/DeepTrenches/core/entity/BlackDragonfishEntity.java
@@ -0,0 +1,12 @@
+package github.KingVampyre.DeepTrenches.core.entity;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.world.World;
+
+public class BlackDragonfishEntity extends BlackLoosejawEntity {
+
+    public BlackDragonfishEntity(EntityType<? extends BlackDragonfishEntity> type, World world) {
+        super(type, world);
+    }
+
+}
