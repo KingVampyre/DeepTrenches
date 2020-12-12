e61087893c2099503e66baf377bd45fe041cbd5c
Louwind <46799905+Louwind@users.noreply.github.com>
2020-12-11 21:14:33 -0800

    Fix: Giant Seed Shrimp Renderer

--- src/main/java/github/KingVampyre/DeepTrenches/client/render/entity/renderer/GiantSeedShrimpRenderer.java
+++ src/main/java/github/KingVampyre/DeepTrenches/client/render/entity/renderer/GiantSeedShrimpRenderer.java
@@ -1,19 +1,19 @@
 package github.KingVampyre.DeepTrenches.client.render.entity.renderer;
 
-import github.KingVampyre.DeepTrenches.client.render.entity.model.GiantHatchetfishModel;
-import github.KingVampyre.DeepTrenches.core.entity.GiantHatchetfishEntity;
+import github.KingVampyre.DeepTrenches.client.render.entity.model.GiantSeedShrimpModel;
+import github.KingVampyre.DeepTrenches.core.entity.GiantSeedShrimpEntity;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.util.Identifier;
 
-public class GiantSeedShrimpRenderer extends MobEntityRenderer<GiantHatchetfishEntity, GiantHatchetfishModel> {
+public class GiantSeedShrimpRenderer extends MobEntityRenderer<GiantSeedShrimpEntity, GiantSeedShrimpModel> {
 
     public GiantSeedShrimpRenderer(EntityRenderDispatcher entityRenderDispatcher) {
-        super(entityRenderDispatcher, new GiantHatchetfishModel(), 0.3F);
+        super(entityRenderDispatcher, new GiantSeedShrimpModel(), 0.3F);
     }
 
     @Override
-    public Identifier getTexture(GiantHatchetfishEntity entity) {
+    public Identifier getTexture(GiantSeedShrimpEntity entity) {
         return null;
     }
 
