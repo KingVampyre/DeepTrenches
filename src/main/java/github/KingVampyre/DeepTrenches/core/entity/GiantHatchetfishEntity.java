6e2f7e04d151ca2c5b106551abb473e52f3f958e
Louwind <mr.louwind@gmail.com>
2021-05-21 22:02:16 -0700

    Fix: Giant Hatchetfish Light Container

--- src/main/java/github/KingVampyre/DeepTrenches/core/entity/GiantHatchetfishEntity.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/entity/GiantHatchetfishEntity.java
@@ -1,6 +1,8 @@
 package github.KingVampyre.DeepTrenches.core.entity;
 
+import com.google.common.collect.ImmutableList;
 import github.KingVampyre.DeepTrenches.common.entity.HatchetfishEntity;
+import github.KingVampyre.DeepTrenches.common.entity.ai.mob.LightState;
 import github.KingVampyre.DeepTrenches.core.init.ModItems;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
@@ -9,6 +11,7 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.world.World;
 
 import static github.KingVampyre.DeepTrenches.core.init.AttributeModifiers.MOVEMENT_SPEED_BOOST_150;
+import static github.KingVampyre.DeepTrenches.core.init.LightStates.*;
 
 public class GiantHatchetfishEntity extends HatchetfishEntity {
 
@@ -19,6 +22,11 @@ public class GiantHatchetfishEntity extends HatchetfishEntity {
 	@Override
 	protected ItemStack getFishBucketItem() {
 		return new ItemStack(ModItems.GIANT_HATCHETFISH_BUCKET);
+	}
+
+	@Override
+	public ImmutableList<LightState> getLightContainer() {
+		return ImmutableList.of(ALL_LIT, ALL_UNLIT);
 	}
 
 	@Override
