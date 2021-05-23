ae7bbbe84bf259cc98f54ebb65a09019d46419d4
Louwind <mr.louwind@gmail.com>
2021-05-23 10:55:14 -0700

    Add: Misc, Sea Blocks & Woods Item Groups

--- src/main/java/github/KingVampyre/DeepTrenches/core/init/ItemGroups.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/init/ItemGroups.java
@@ -5,12 +5,22 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
+import static github.KingVampyre.DeepTrenches.core.init.ModBlocks.FUCHSITRA_LOG;
 import static github.KingVampyre.DeepTrenches.core.init.ModBlocks.PIPE_ORGAN_CORAL;
+import static github.KingVampyre.DeepTrenches.core.init.ModItems.BARBELED_LOOSEJAW;
 
 public class ItemGroups {
 
-    public static final ItemGroup GENERAL = FabricItemGroupBuilder.build(
-            new Identifier("deep_trenches:general"),
+    public static final ItemGroup MISC = FabricItemGroupBuilder.build(
+            new Identifier("deep_trenches:misc"),
+            () -> new ItemStack(BARBELED_LOOSEJAW));
+
+    public static final ItemGroup SEA_BLOCKS = FabricItemGroupBuilder.build(
+            new Identifier("deep_trenches:sea_blocks"),
             () -> new ItemStack(PIPE_ORGAN_CORAL));
+
+    public static final ItemGroup WOODS = FabricItemGroupBuilder.build(
+            new Identifier("deep_trenches:sea_blocks"),
+            () -> new ItemStack(FUCHSITRA_LOG));
 
 }
