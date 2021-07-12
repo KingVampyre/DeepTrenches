5acbf5e2f8ae3888893ec8c9bf8564de0c402b63
Louwind <mr.louwind@gmail.com>
2021-07-11 23:55:42 -0700

    Fix: Biome Surface

--- src/main/java/github/KingVampyre/DeepTrenches/core/world/gen/chunk/TheDreamChunkGenerator.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/world/gen/chunk/TheDreamChunkGenerator.java
@@ -1,12 +1,15 @@
 package github.KingVampyre.DeepTrenches.core.world.gen.chunk;
 
+import com.google.common.collect.Lists;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import github.KingVampyre.DeepTrenches.core.world.gen.TheDreamBlockSource;
 import github.KingVampyre.DeepTrenches.core.world.gen.chunk.aquifier.TheDreamAquiferSampler;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.dynamic.RegistryLookupCodec;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
@@ -14,9 +17,9 @@ import net.minecraft.world.ChunkRegion;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.source.BiomeSource;
 import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.gen.BlockSource;
-import net.minecraft.world.gen.ChunkRandom;
-import net.minecraft.world.gen.NoiseInterpolator;
+import net.minecraft.world.chunk.ChunkSection;
+import net.minecraft.world.chunk.ProtoChunk;
+import net.minecraft.world.gen.*;
 import net.minecraft.world.gen.chunk.AquiferSampler;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
@@ -28,6 +31,7 @@ import java.util.function.DoubleFunction;
 import java.util.function.Supplier;
 
 import static net.minecraft.util.registry.Registry.BIOME_KEY;
+import static net.minecraft.world.Heightmap.Type.OCEAN_FLOOR_WG;
 import static net.minecraft.world.Heightmap.Type.WORLD_SURFACE_WG;
 import static net.minecraft.world.gen.chunk.ChunkGeneratorSettings.REGISTRY_CODEC;
 
@@ -63,17 +67,17 @@ public class TheDreamChunkGenerator extends NoiseChunkGenerator {
         this.terrain = terrain;
     }
 
-    public BlockSource getBlockSource(Biome biome) {
+    protected BlockSource getBlockSource(Biome biome) {
         var settings = this.settings.get();
 
         return new TheDreamBlockSource(() -> this.getTerrainBlockState(biome), this.seed, settings);
     }
 
-    public BlockState getFluidBlockState(Biome biome) {
+    protected BlockState getFluidBlockState(Biome biome) {
         return this.biomeRegistry.getKey(biome).map(this.fluids::get).orElse(this.defaultFluid);
     }
 
-    public BlockState getTerrainBlockState(Biome biome) {
+    protected BlockState getTerrainBlockState(Biome biome) {
         return this.biomeRegistry.getKey(biome).map(this.terrain::get).orElse(this.defaultBlock);
     }
 
@@ -87,6 +91,8 @@ public class TheDreamChunkGenerator extends NoiseChunkGenerator {
         var chunkPosZ = chunkPos.z;
         var yScale = 0.0625D;
 
+        var mutable = new BlockPos.Mutable();
+
         chunkRandom.setTerrainSeed(chunkPosX, chunkPosZ);
 
         for(var sectionX = 0; sectionX < 16; ++sectionX) {
@@ -97,11 +103,13 @@ public class TheDreamChunkGenerator extends NoiseChunkGenerator {
                 var noise = this.surfaceDepthNoise.sample(x * yScale, z * yScale, yScale, sectionX * yScale) * 15.0D;
                 var height = chunk.sampleHeightmap(WORLD_SURFACE_WG, sectionX, sectionZ) + 1;
                 var minSurfaceLevel = this.settings.get().getMinSurfaceLevel();
-                var biome = this.populationSource.getBiomeForNoiseGen(chunkPos);
-                var defaultState = this.getTerrainBlockState(biome);
+
+                var biome = region.getBiome(mutable.set(x, height, z));
+
+                var defaultBlock = this.getTerrainBlockState(biome);
                 var defaultFluid = this.getFluidBlockState(biome);
 
-                biome.buildSurface(chunkRandom, chunk, x, z, height, noise, defaultState, defaultFluid, this.getSeaLevel(), minSurfaceLevel, region.getSeed());
+                biome.buildSurface(chunkRandom, chunk, x, z, height, noise, defaultBlock, defaultFluid, this.getSeaLevel(), minSurfaceLevel, region.getSeed());
             }
 
         }
@@ -121,8 +129,103 @@ public class TheDreamChunkGenerator extends NoiseChunkGenerator {
     }
 
     @Override
-    public DoubleFunction<BlockSource> createBlockSourceFactory(int minY, ChunkPos pos, Consumer<NoiseInterpolator> consumer) {
-        var biome = this.populationSource.getBiomeForNoiseGen(pos);
+    public Chunk populateNoise(StructureAccessor accessor, Chunk chunk, int startY, int noiseSizeY) {
+        var oceanFloor = chunk.getHeightmap(OCEAN_FLOOR_WG);
+        var worldSurface = chunk.getHeightmap(WORLD_SURFACE_WG);
+
+        var chunkPos = chunk.getPos();
+        var startX = chunkPos.getStartX();
+        var startZ = chunkPos.getStartZ();
+
+        var structureWeightSampler = new StructureWeightSampler(accessor, chunk);
+        var noiseInterpolator = new NoiseInterpolator(this.noiseSizeX, noiseSizeY, this.noiseSizeZ, chunkPos, startY, this::sampleNoiseColumn);
+
+        var interpolators = Lists.newArrayList(noiseInterpolator);
+        var aquiferSampler = this.createBlockSampler(startY, noiseSizeY, chunkPos);
+        var weightSamplerFactory = this.createWeightSamplerFactory(startY, chunkPos, interpolators::add);
+
+        interpolators.forEach(NoiseInterpolator::sampleStartNoise);
+
+        var mutable = new BlockPos.Mutable();
+
+        for(var noiseX = 0; noiseX < this.noiseSizeX; ++noiseX) {
+
+            for(var interpolator : interpolators)
+                interpolator.sampleEndNoise(noiseX);
+
+            for(var noiseZ = 0; noiseZ < this.noiseSizeZ; ++noiseZ) {
+                ChunkSection chunkSection = chunk.getSection(chunk.countVerticalSections() - 1);
+
+                for(var noiseY = noiseSizeY - 1; noiseY >= 0; --noiseY) {
+
+                    for(var interpolator : interpolators)
+                        interpolator.sampleNoiseCorners(noiseY, noiseZ);
+
+                    for(var sectionY = this.verticalNoiseResolution - 1; sectionY >= 0; --sectionY) {
+                        var posY = (startY + noiseY) * this.verticalNoiseResolution + sectionY;
+                        var y = posY & 15;
+
+                        var sectionIndex = chunk.getSectionIndex(posY);
+
+                        if (chunk.getSectionIndex(chunkSection.getYOffset()) != sectionIndex)
+                            chunkSection = chunk.getSection(sectionIndex);
+
+                        var deltaY = (double) sectionY / (double) this.verticalNoiseResolution;
+
+                        for(var interpolator : interpolators)
+                            interpolator.sampleNoiseY(deltaY);
+
+                        for(var sectionX = 0; sectionX < this.horizontalNoiseResolution; ++sectionX) {
+                            var posX = startX + noiseX * this.horizontalNoiseResolution + sectionX;
+                            var x = posX & 15;
+                            var deltaX = (double) sectionX / (double) this.horizontalNoiseResolution;
+
+                            for(var interpolator : interpolators)
+                                interpolator.sampleNoiseX(deltaX);
+
+                            for(var sectionZ = 0; sectionZ < this.horizontalNoiseResolution; ++sectionZ) {
+                                var posZ = startZ + noiseZ * this.horizontalNoiseResolution + sectionZ;
+                                var z = posZ & 15;
+                                var deltaZ = (double) sectionZ / (double) this.horizontalNoiseResolution;
+                                var weight = noiseInterpolator.sampleNoise(deltaZ);
+
+                                var biome = accessor.world.getBiome(mutable.set(posX, posY, posZ));
+                                var blockFactory = this.createTerrainFactory(startY, biome, chunkPos, interpolators::add);
+                                var state = this.getBlockState(structureWeightSampler, aquiferSampler, blockFactory.apply(deltaZ), weightSamplerFactory.apply(deltaZ), posX, posY, posZ, weight);
+
+                                if (state != Blocks.AIR.getDefaultState()) {
+
+                                    if (state.getLuminance() != 0 && chunk instanceof ProtoChunk)
+                                        ((ProtoChunk)chunk).addLightSource(mutable);
+
+                                    chunkSection.setBlockState(x, y, z, state, false);
+                                    oceanFloor.trackUpdate(x, posY, z, state);
+                                    worldSurface.trackUpdate(x, posY, z, state);
+
+                                    if (aquiferSampler.needsFluidTick() && !state.getFluidState().isEmpty()) {
+                                        mutable.set(posX, posY, posZ);
+                                        chunk.getFluidTickScheduler().schedule(mutable, state.getFluidState().getFluid(), 0);
+                                    }
+
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+            interpolators.forEach(NoiseInterpolator::swapBuffers);
+        }
+
+        return chunk;
+    }
+
+    protected DoubleFunction<BlockSource> createTerrainFactory(int minY, Biome biome, ChunkPos pos, Consumer<NoiseInterpolator> consumer) {
         var source = this.getBlockSource(biome);
 
         if (!this.settings.get().hasOreVeins())
