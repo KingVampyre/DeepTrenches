dcac7ec234792573a54b4d74ebd804978d6d3bf3
Louwind <46799905+Louwind@users.noreply.github.com>
2021-09-15 17:52:48 -0700

    Refactor: Worldgen Clean Up

--- src/main/java/github/KingVampyre/DeepTrenches/core/DeepTrenches.java
+++ src/main/java/github/KingVampyre/DeepTrenches/core/DeepTrenches.java
@@ -1,19 +1,15 @@
 package github.KingVampyre.DeepTrenches.core;
 
-import github.KingVampyre.DeepTrenches.client.init.DTGeneratorTypes;
 import github.KingVampyre.DeepTrenches.common.event.block.BlockReplacementCallback;
 import github.KingVampyre.DeepTrenches.core.event.block.AirialMossCallback;
 import github.KingVampyre.DeepTrenches.core.event.block.IlmiumCallback;
 import github.KingVampyre.DeepTrenches.core.event.entity.AllowSleepyStatusEffect;
 import github.KingVampyre.DeepTrenches.core.init.*;
-import github.KingVampyre.DeepTrenches.core.mixin.AccessorGeneratorType;
-import github.KingVampyre.DeepTrenches.core.world.gen.chunk.TheDreamChunkGenerator;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
 import net.minecraft.util.registry.Registry;
 import software.bernie.geckolib3.GeckoLib;
 
-import static net.minecraft.util.registry.BuiltinRegistries.CHUNK_GENERATOR_SETTINGS;
 import static net.minecraft.util.registry.Registry.*;
 
 public class  DeepTrenches implements ModInitializer {
@@ -29,9 +25,6 @@ public class  DeepTrenches implements ModInitializer {
 		DTConfiguredFeatures.initialize();
 		DTBiomes.initialize();
 
-		/* GENERATOR TYPE */
-		AccessorGeneratorType.getValues().add(DTGeneratorTypes.THE_DREAM);
-
 		/* EVENT */
 		BlockReplacementCallback.EVENT.register(AirialMossCallback.INSTANCE);
 		BlockReplacementCallback.EVENT.register(IlmiumCallback.INSTANCE);
@@ -40,12 +33,6 @@ public class  DeepTrenches implements ModInitializer {
 		/* BLOCK ENTITY */
 		Registry.register(BLOCK_ENTITY_TYPE, "deep_trenches:sign", DTBlockEntityTypes.SIGN);
 		Registry.register(BLOCK_ENTITY_TYPE, "deep_trenches:stasp_nest", DTBlockEntityTypes.STASP_NEST);
-
-		/* CHUNK GENERATOR */
-		Registry.register(CHUNK_GENERATOR, "deep_trenches:the_dream", TheDreamChunkGenerator.CODEC);
-
-		/* CHUNK GENERATOR SETTINGS */
-		Registry.register(CHUNK_GENERATOR_SETTINGS, "deep_trenches:the_dream", DTChunkGeneratorSettings.THE_DREAM);
 
 		/* ENCHANTMENT */
 		Registry.register(ENCHANTMENT, "deep_trenches:soul_draining", DTEnchantments.SOUL_DRAINING);
